c4005c1e324f2e56e8853618c52b18dd20ef4d8f
Ali Mahjoub <mahjoubali@seliana.r-iset.tn>
2024-02-13 23:24:58 +0100

    update

--- frontPFE/src/app/login/login.component.ts
+++ frontPFE/src/app/login/login.component.ts
@@ -3,11 +3,13 @@ import { CheckboxModule } from 'primeng/checkbox';
 import {StyleClassModule} from 'primeng/styleclass';
 import { PasswordModule } from 'primeng/password';
 import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CheckboxModule,StyleClassModule,PasswordModule,ButtonModule],
+  imports: [CheckboxModule,StyleClassModule,PasswordModule,
+    ButtonModule,InputTextModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
