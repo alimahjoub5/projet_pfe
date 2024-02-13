c578f3e7aac8651312475dd6194870ee57f6fdb8
Ali Mahjoub <mahjoubali@seliana.r-iset.tn>
2024-02-13 23:49:08 +0100

    Update login.component.html

--- frontPFE/src/app/login/login.component.ts
+++ frontPFE/src/app/login/login.component.ts
@@ -1,18 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CheckboxModule } from 'primeng/checkbox';
 import {StyleClassModule} from 'primeng/styleclass';
 import { PasswordModule } from 'primeng/password';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastModule } from 'primeng/toast';
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [CheckboxModule,StyleClassModule,PasswordModule,
-    ButtonModule,InputTextModule],
+    ButtonModule,InputTextModule,ToastModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  @ViewChild('password') passwordInput: any; // Accéder à l'élément HTML du mot de passe
 
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  errorMessage: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    // Valider le formulaire
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    // Vérifier si le champ "password" est vide
+    if (!this.passwordInput.nativeElement.value) {
+      this.errorMessage = 'Le mot de passe est obligatoire.';
+      return;
+    }
+
+    // Soumettre le formulaire (implémentez votre logique ici)
+    console.log('Formulaire soumis');
+  }
 }
