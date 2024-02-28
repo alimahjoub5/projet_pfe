3cf11a5264b712b8db07d08de41e818b7aa66e00
OumaimaHouimell <116652414+OumaimaHouimell@users.noreply.github.com>
2024-02-28 21:26:58 +0100

    test

--- pfe/src/app/app-routing.module.ts
+++ pfe/src/app/app-routing.module.ts
@@ -19,11 +19,12 @@ import { TestComponent } from './test/test.component';
                     { path: "", loadChildren: () => import('./core/dashboard/dashboard.module').then(m => m.DashboardModule) },
                     {path:'test' ,component : TestComponent},
                     { path: "", loadChildren: () => import('./core/dashboard/tickets/tickets-routing.module').then(m => m.DashboardRoutingModule) },
-                    { path: "", loadChildren: () => import('./core/dashboard/administrateur/administrateur-routing.module').then(m => m.AdministrateurRoutingModule) }
+                    { path: "", loadChildren: () => import('./core/dashboard/administrateur/administrateur-routing.module').then(m => m.AdministrateurRoutingModule) },
 
                 ]
             },
-            { path: 'auth', loadChildren: () => import('./demo/components/auth/auth.module').then(m => m.AuthModule) },
+            { path: 'authen', loadChildren: () => import('./core/auth/auth-routing.module').then(m => m.AuthRoutingModule) },
+
             { path: 'landing', loadChildren: () => import('./demo/components/landing/landing.module').then(m => m.LandingModule) },
             { path: 'notfound', component: NotfoundComponent },
             { path: '**', redirectTo: '/notfound' },
