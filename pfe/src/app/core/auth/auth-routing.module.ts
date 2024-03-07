977ed3c4981c890c88d82facc2e853f911057058
OumaimaHouimell <116652414+OumaimaHouimell@users.noreply.github.com>
2024-03-07 12:21:49 +0100

    .html

--- pfe/src/app/core/auth/auth-routing.module.ts
+++ pfe/src/app/core/auth/auth-routing.module.ts
@@ -1,31 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-import { ProfileComponent } from './profile/profile.component';
 import { RequestResetComponent } from './password/request-reset/request-reset.component';
+import { ProfileComponent } from './profile/profile.component';
 import { ResponseResetComponent } from './password/response-reset/response-reset.component';
 
 const routes: Routes = [
   {
     path: 'login',
-    component: LoginComponent,
+    component: LoginComponent
   },
-  {
-    path: 'signup',
-    component: SignupComponent,
-  },
+
   {
     path: 'profile',
-    component: ProfileComponent,
+    component: ProfileComponent
+    
   },
   {
     path: 'request-password-reset',
-    component: RequestResetComponent,
+    component: RequestResetComponent
+ 
   },
   {
     path: 'response-password-reset',
-    component: ResponseResetComponent,
+    component: ResponseResetComponent
+    
   },
 ];
 
--- pfe/src/app/core/auth/password/request-reset/request-reset.component.ts
+++ pfe/src/app/core/auth/password/request-reset/request-reset.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-
+import { Router,RouterModule } from '@angular/router';
 @Component({
   selector: 'app-request-reset',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule
+  ],
   templateUrl: './request-reset.component.html',
   styleUrl: './request-reset.component.scss'
 })
