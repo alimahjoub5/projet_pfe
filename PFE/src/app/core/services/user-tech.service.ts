d3e25ef4d5602918264c04ca26065a2dd711e451
Ali Mahjoub <mahjoubali@seliana.r-iset.tn>
2024-03-27 12:03:45 +0100

    Update user-tech.service.ts

--- PFE/src/app/core/services/user-tech.service.ts
+++ PFE/src/app/core/services/user-tech.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Usertech } from '../models/user-tech';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserTechService {
+export class UsersTechnicianGroupsService {
 
-  constructor() { }
+  private baseUrl = 'localhost:8000/api'; // Remplacez ceci par l'URL de votre API
+
+  constructor(private http: HttpClient, private authService: AuthService) { }
+
+  getUsersTechnicianGroups(): Observable<Usertech> {
+    const headers = this.authService.includeAuthToken();
+
+    return this.http.get(`${this.baseUrl}/users-technician-groups`);
+  }
+
+  getUserTechnicianGroup(groupId: number): Observable<Usertech> {
+    const headers = this.authService.includeAuthToken();
+
+    return this.http.get(`${this.baseUrl}/users-technician-groups/${groupId}`,headers);
+  }
+
+  assignUserToGroup(userId: number, groupId: number): Observable<Usertech> {
+    const headers = this.authService.includeAuthToken();
+
+    return this.http.post(`${this.baseUrl}/users-technician-groups/assign`, { UserID: userId, GroupID: groupId },headers);
+  }
+
+  removeUserFromGroup(userId: number, groupId: number): Observable<Usertech> {
+    const headers = this.authService.includeAuthToken();
+
+    return this.http.post(`${this.baseUrl}/users-technician-groups/remove`, { UserID: userId, GroupID: groupId },headers);
+  }
+
+  getUsersInGroup(groupId: number): Observable<Usertech> {
+    const headers = this.authService.includeAuthToken();
+
+    return this.http.get(`${this.baseUrl}/users-technician-groups/${groupId}/users`,headers);
+  }
 }
