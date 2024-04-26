9e1dfcbf2e5bda3b726e4fd41fc31784a991b7ca
OumaimaHouimell <116652414+OumaimaHouimell@users.noreply.github.com>
2024-04-26 13:41:14 +0200

    Merge branch 'main' of https://github.com/alimahjoub5/projet_pfe

--- PFE/src/app/core/GestionDeStocks/utilisation/crudutilisation/addutilisation/addutilisation.component.ts
+++ PFE/src/app/core/GestionDeStocks/utilisation/crudutilisation/addutilisation/addutilisation.component.ts
@@ -24,79 +24,74 @@ import { PieceService } from 'src/app/core/services/GestionDeStocks/pieceService
     NgxSpinnerModule, 
   ToastModule],
   templateUrl: './addutilisation.component.html',
-  styleUrl: './addutilisation.component.scss'
+  styleUrls: ['./addutilisation.component.scss']
 })
 export class AddutilisationComponent implements OnInit {
   utilisationform: FormGroup;
-  isLoading : boolean;
+  isLoading: boolean;
   equipements: EquipmentType[] = [];
-  selectedEquipement: number | undefined;
-
- 
-
   pieces: Piece[] = [];
-  selectedPiece: number | undefined;
+
   constructor(
     private fb: FormBuilder,
-  
     private equipmentService: EquipmentTypeService,
     private spinner: NgxSpinnerService,
     private messageService: MessageService,
     private authservice: AuthService,
-    private pieceservice:PieceService,
-    private utilisationservice:UtilisationPieceService
+    private pieceservice: PieceService,
+    private utilisationservice: UtilisationPieceService
   ) {}
-
-  showSuccess() {
-    this.messageService.add({ severity: 'success', summary: 'Succès', detail: 'Utilisation créé avec succès' });
-  }
-
   ngOnInit(): void {
     this.utilisationform = this.fb.group({
-     
-      equipmentTypeID: [null, Validators.required],
+      EquipmentTypeID: [null, Validators.required],
       piece_id: [null, Validators.required],
-      quantity_used: ['', Validators.required], 
-       date_utilisation: ['', Validators.required],
+      quantity_used: ['', Validators.required],
+      date_utilisation: ['', Validators.required],
       description: [''],
-
     });
-
+    
     this.getAllEquipements();
-    this.getAllPicess();
-
+    this.getAllPieces();
   }
+
   getAllEquipements(): void {
-    this.equipmentService.getAllEquipmentTypes().subscribe(equipements => {
+    this.equipmentService.getAllEquipmentTypes().subscribe((equipements: EquipmentType[]) => {
       this.equipements = equipements;
     });
   }
 
-  getAllPicess(): void {
-    this.pieceservice.getAllPieces().subscribe(pieces => {
+  getAllPieces(): void {
+    this.pieceservice.getAllPieces().subscribe((pieces: Piece[]) => {
       this.pieces = pieces;
     });
   }
 
   onSubmit(): void {
-    const formData = new FormData();
-    formData.append('EquipmentTypeID', this.utilisationform.get('EquipmentTypeID').value);
-    formData.append('piece_id', this.utilisationform.get('piece_id').value);
-    formData.append('quantity_used', this.utilisationform.get('quantity_used').value);
-    formData.append('date_utilisation', this.utilisationform.get('date_utilisation').value);
-    formData.append('description', this.utilisationform.get('description').value);
-   
+    if (this.utilisationform.valid) {
+      const formData = new FormData();
+      formData.append('EquipmentTypeID', this.utilisationform.get('EquipmentTypeID').value);
+      formData.append('piece_id', this.utilisationform.get('piece_id').value);
+      formData.append('quantity_used', this.utilisationform.get('quantity_used').value);
+      formData.append('date_utilisation', this.utilisationform.get('date_utilisation').value);
+      formData.append('description', this.utilisationform.get('description').value);
 
-
-    this.utilisationservice.createUtilisationPiece(formData).subscribe(
-      (response) => {
-        console.log('Piece créée avec succès :', response);
-        this.utilisationform.reset();
-      },
-      (error) => {
-        console.error('Erreur lors de la création de la pièce :', error);
-      }
-    );
+      this.isLoading = true;
+      this.utilisationservice.createUtilisationPiece(formData).subscribe(
+        (response: any) => {
+          console.log('Utilisation créée avec succès :', response);
+          this.utilisationform.reset();
+          this.isLoading = false;
+          this.showSuccess(); // Call the success message method
+        },
+        (error: any) => {
+          console.error('Erreur lors de la création de l\'utilisation :', error);
+          this.isLoading = false;
+        }
+      );
+    }
   }
 
+  showSuccess(): void {
+    this.messageService.add({ severity: 'success', summary: 'Succès', detail: 'Utilisation créée avec succès' });
+  }
 }
