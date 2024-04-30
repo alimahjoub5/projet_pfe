42892d60b2389900df4ae5709b01f06081c7e675
OumaimaHouimell <116652414+OumaimaHouimell@users.noreply.github.com>
2024-04-30 16:50:46 +0200

    addtask

--- PFE/src/app/core/dashboard/task/task-routing.module.ts
+++ PFE/src/app/core/dashboard/task/task-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
     component: CreatetasksComponent 
   },
   {
-    path: "updatetasks/:TicketTaskID",
+    path: "updatetasks/:id",
     component: UpdatetasksComponent
   },
   {
